a9252ece5273ad000ae183ae52b2e26b6f124a15
kareemIt <kareem.itani2@gmail.com>
2022-02-02 10:01:30 -0500

    working on comparing feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Gameboard from './components/Gameboard.js';
 
 export default function App() {
   const [numberLetters, useNumberLetters] = useState(5);
+  const [row, setRow] = useState(0);
   const [currentPosition, setCurrentPosition] = useState(0);
+  const word = 'hello';
   const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
   const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
   const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
@@ -38,6 +40,7 @@ export default function App() {
   ];
 
   document.addEventListener('keydown', (event) => {
+    if (event.key == 'ENTER' && numberLetters == 0) sumbited;
     if (numberLetters == 0) return;
     if (event.key == 'BACKSPACE');
     const currentKey = event.key.toUpperCase();
@@ -46,6 +49,12 @@ export default function App() {
     console.log(currentPosition);
     console.log(currentKey);
   });
+  const sumbited = () => {
+    //check the board and compare it to the word
+    //then add grey/yellow/green class 
+    //reset numberLetters
+  };
+
   return (
     <div>
       <h1>Wordle</h1>
