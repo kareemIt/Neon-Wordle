f50e4481d24558507e245097ed14cee0314aa17c
kareemIt <kareem.itani2@gmail.com>
2022-02-02 09:05:29 -0500

    working on adding to gameboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gameboard from './components/Gameboard.js';
 
 export default function App() {
   const [numberLetters, useNumberLetters] = useState(5);
+  const [currentPosition, setCurrentPosition] = useState(0);
   const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
   const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
   const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
@@ -36,13 +37,14 @@ export default function App() {
     '',
   ];
   useEffect(() => {
-    console.log(board);
-  }, [board]);
+    setCurrentPosition(currentPosition + 1);
+  }, [currentPosition]);
   document.addEventListener('keydown', (event) => {
     const currentKey = event.key.toUpperCase();
-    board.push(currentKey);
+    board[currentPosition] = currentKey;
+    setCurrentPosition(currentPosition + 1);
+    console.log(currentPosition);
     console.log(board);
-    console.log(currentKey);
   });
   return (
     <div>
