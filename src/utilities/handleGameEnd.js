f8fdc56f324be86a42ee8a442af8615f3307052c
kareemIt <kareem.itani2@gmail.com>
2022-03-16 11:00:58 -0400

    fixed end game results

--- src/utilities/handleGameEnd.js
+++ src/utilities/handleGameEnd.js
@@ -1,10 +1,13 @@
 export const handleGameEnd = (userString, word, currentRow) => {
   for (let i = 0; i < userString.length; i++) {
-    if (userString[i] == word[i] && currentRow == 5) {
-      return -1;
+    if (userString == word && currentRow == 5) {
+      return 1;
     }
     if (userString == word && currentRow < 5) {
       return 1;
+    }
+    if (userString[i] == word[i] && currentRow == 5) {
+      return -1;
     }
   }
 };
